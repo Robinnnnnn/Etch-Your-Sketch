6766d697b4c863185945f53aef05dc9d6f2538a3
Your NameRobin <rlifshitz19@gmail.com>
2019-12-25 23:19:17 -0700

    set grid dimensions working

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-let container = document.getElementById('gameContainer');
-let size = 63;
+let container = document.getElementById("gameContainer");
+let gridSizeBtn = document.getElementById("changeDim");
+let size;
 
 //sets the initial size of the game container.
 //Functionality to change the sizes will be added.
-container.setAttribute('style', `display: grid; grid-template-columns:repeat(${size}, 1fr); grid-template-rows: repeat(${size}, 1fr);`);
-
+container.setAttribute(
+    "style",
+    `display: grid; grid-template-columns:repeat(${size}, 1fr); grid-template-rows: repeat(${size}, 1fr);`
+);
 
 //creates the grids to be added to the container
 function createGrids(size) {
-
     for (let i = 1; i <= size; i++) {
-        let aDiv = document.createElement('div');
-        aDiv.classList.add('grids');
-        aDiv.setAttribute('id', 'div' + i);
-        aDiv.setAttribute('style', `grid-column-start: ${i}; grid-row-start: ${i}; grid-column-end: ${i}+1 ; grid-row-end: ${i}+1 ;`);
+        let aDiv = document.createElement("div");
+        aDiv.classList.add("grids");
+        aDiv.setAttribute("id", "div" + i);
+        aDiv.setAttribute(
+            "style",
+            `grid-column-start: ${i}; grid-row-start: ${i}; grid-column-end: ${i}+1 ; grid-row-end: ${i}+1 ;`
+        );
         container.append(aDiv);
     }
-};
+}
 
-createGrids();
+function assignDim() {
+    size = window.prompt("Assign Grid Dimensions", "A # 2-100");
+    createGrids(size);
+}
+
+// gridSizeBtn.onclick(window.prompt("Dimensions of Grid"));
