8d3a425ecc0fbc1a5924a4f070c63aa3b5c188ef
Your NameRobin <rlifshitz19@gmail.com>
2019-12-26 21:32:25 -0700

    Final grids/Divs working

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let container = document.getElementById("gameContainer");
 let gridSizeBtn = document.getElementById("changeDim");
+let color = "rgb(0, 0, 0)";
+let squares = document.getElementsByClassName("grids");
 let size;
+let aDiv;
 
 //sets the initial size of the game container.
 //Functionality to change the sizes will be added.
@@ -9,18 +12,35 @@ container.setAttribute(
     `display: grid; grid-template-columns:repeat(${size}, 1fr); grid-template-rows: repeat(${size}, 1fr);`
 );
 
+//assigns individual Id's for each div
+function assignId() {
+    for (let n = 1; n <= squares.length; n++) {
+        aDiv.setAttribute("id", "div" + n);
+    }
+}
+
 //creates the grids to be added to the container
 function createGrids(size) {
-    for (let i = 1; i <= size; i++) {
-        let aDiv = document.createElement("div");
-        aDiv.classList.add("grids");
-        aDiv.setAttribute("id", "div" + i);
-        aDiv.setAttribute(
-            "style",
-            `grid-column-start: ${i}; grid-row-start: ${i}; grid-column-end: ${i}+1 ; grid-row-end: ${i}+1 ;`
-        );
-        container.append(aDiv);
+    for (let j = 1; j <= size; j++) {
+        for (let i = 1; i <= size; i++) {
+            aDiv = document.createElement("div");
+            aDiv.classList.add("grids");
+            aDiv.setAttribute(
+                "style",
+                `grid-column-start: ${j}; grid-row-start: ${i}; grid-column-end: ${j}+1 ; grid-row-end: ${i}+1 ;`
+            );
+            container.append(aDiv);
+        }
     }
+
+    //assigns individual Id's for each div
+    function assignId() {
+        let individuals = Array.from(squares);
+        for (let n = 1; n <= individuals.length; n++) {
+            individuals[n - 1].setAttribute("id", "div" + n);
+        }
+    }
+    assignId();
 }
 
 function assignDim() {
@@ -29,9 +49,6 @@ function assignDim() {
 }
 
 //on hover find the id of the div and changes the bacground color to black
-let color = "rgb(0, 0, 0)";
-let squares = document.getElementsByClassName("grids");
-
 function colorGrid(color) {
     for (let i = 0; i < squares.length; i++) {
         squares[i].addEventListener("mouseover", function(event) {
