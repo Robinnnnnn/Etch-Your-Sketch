8e40a8e390e78bff4e582b671ef4310c3d5205e0
Your NameRobin <rlifshitz19@gmail.com>
2019-12-28 17:01:29 -0700

    Grid dimensions changes completed. Coloring of grids and reseting of color completed.

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 let container = document.getElementById("gameContainer");
 let gridSizeBtn = document.getElementById("changeDim");
-let color = "rgb(0, 0, 0)";
+let color = "black";
 let squares = document.getElementsByClassName("grids");
-let size;
+let clear = document.getElementById("clearGrid");
+let size = 0;
 let aDiv;
 
 //sets the initial size of the game container.
-//Functionality to change the sizes will be added.
 container.setAttribute(
     "style",
     `display: grid; grid-template-columns:repeat(${size}, 1fr); grid-template-rows: repeat(${size}, 1fr);`
 );
 
-//assigns individual Id's for each div
-function assignId() {
-    for (let n = 1; n <= squares.length; n++) {
-        aDiv.setAttribute("id", "div" + n);
-    }
+//gets user response for grid dimensions & removes previous grids.
+function assignDim() {
+    size = window.prompt("Assign Grid Dimensions", "A number between 2-100");
+    gridReset();
+    createGrids(size);
 }
 
-//creates the grids to be added to the container
+//creates the grids to be added to the container and assigns individual Id's
 function createGrids(size) {
     for (let j = 1; j <= size; j++) {
         for (let i = 1; i <= size; i++) {
@@ -27,34 +27,35 @@ function createGrids(size) {
             aDiv.classList.add("grids");
             aDiv.setAttribute(
                 "style",
-                `grid-column-start: ${j}; grid-row-start: ${i}; grid-column-end: ${j}+1 ; grid-row-end: ${i}+1 ;`
+                `grid-column-start: ${j}; grid-row-start: ${i}; grid-column-end: ${j}+1 ; grid-row-end: ${i}+1 ; background-color:white;`
             );
             container.append(aDiv);
         }
     }
 
-    //assigns individual Id's for each div
-    function assignId() {
-        let individuals = Array.from(squares);
-        for (let n = 1; n <= individuals.length; n++) {
-            individuals[n - 1].setAttribute("id", "div" + n);
-        }
-    }
     assignId();
 }
 
-function assignDim() {
-    size = window.prompt("Assign Grid Dimensions", "A # 2-100");
-    createGrids(size);
+//assigns individual Id's for each div & hover effect
+function assignId() {
+    let individuals = Array.from(squares);
+    for (let n = 1; n <= individuals.length; n++) {
+        individuals[n - 1].setAttribute("id", "div" + n);
+        individuals[n - 1].addEventListener("mouseover", function(event) {
+            event.target.style.backgroundColor = color;
+        });
+    }
 }
 
-//on hover find the id of the div and changes the bacground color to black
-function colorGrid(color) {
-    for (let i = 0; i < squares.length; i++) {
-        squares[i].addEventListener("mouseover", function(event) {
-            let chosenSquare = event.target.getAttribute("id");
-            let chosenSquare1 = document.getElementById(`#${chosenSquare}`);
-            chosenSquare1.setAttribute(`background-color: ${color} `);
-        });
+function clearGrid() {
+    let individuals = Array.from(squares);
+    for (let i = 0; i < individuals.length; i++) {
+        individuals[i].style.backgroundColor = "white";
+    }
+}
+
+function gridReset() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
     }
 }
